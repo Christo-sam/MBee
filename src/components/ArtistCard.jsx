663b12e7d4335787504ab14c40ee8521800d1780
christo-sam <samchristo562@gmail.com>
2022-11-17 20:13:16 +0530

    features added

---
+++ src/components/ArtistCard.jsx
@@ -0,0 +1,28 @@
+import { Link, useNavigate } from "react-router-dom";
+
+const ArtistCard = ({track}) => {
+  const navigate = useNavigate();
+  console.log(track);
+
+  return(
+    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 
+         backdrop-blur-sm animate-slideup rounded-full hover:bg-white 
+         transition-all delay-12  cursor-pointer
+         bg-gradient-to-tr from-transparent to-black"
+      onClick={()=>navigate(`/artists/${track?.artists[0].adamid}`)}
+      >
+        <img 
+          className="w-full h-56 rounded-full"
+          src={track?.images?.background}
+          alt="artist"
+        />    
+    </div>
+  )
+};
+
+export default ArtistCard;
+
+
+{/* <div className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${track?.subtitle === track.subtitle ? 'flex bg-black bg-opacity-70':'hidden'}`}>
+          <p>{track.subtitle}</p>
+        </div> */}
